d2140961f12ac2262edec0aa882f6f5deca78dcb
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-03-20 08:53:57 +0100

    release v0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.smee/archive "0.2.0-SNAPSHOT"
+(defproject org.clojars.smee/archive "0.2.0"
   :description "FIXME: write"
   :dev-dependencies  [] 
   :dependencies [[org.clojure/clojure "1.3.0"]])
